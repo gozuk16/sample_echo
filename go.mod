7888fda5d626b50e53e450b532a357e762aa48ec
GOZU Kenichiro <gozuk16@gmail.com>
2021-07-10 17:47:51 +0900

    WebSocketのサンプルを追加

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/gozuk16/sample_echo
 go 1.16
 
 require (
-	github.com/labstack/echo v3.3.10+incompatible
+	github.com/gorilla/websocket v1.4.2
 	github.com/labstack/echo/v4 v4.3.0
 	github.com/stretchr/testify v1.7.0 // indirect
 	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
--- main.go
+++ main.go
@@ -1,11 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
+
+var (
+	upgrader = websocket.Upgrader{}
+)
+
+func hello(c echo.Context) error {
+	u := c.Param("user")
+	return c.String(http.StatusOK, "hello, "+u)
+}
+
+func helloWs(c echo.Context) error {
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		return err
+	}
+	defer ws.Close()
+
+	for {
+		// Write
+		err := ws.WriteMessage(websocket.TextMessage, []byte("hello, ws client"))
+		if err != nil {
+			c.Logger().Error(err)
+		}
+
+		// Read
+		_, msg, err := ws.ReadMessage()
+		if err != nil {
+			c.Logger().Error(err)
+		}
+		fmt.Printf("%s\n", msg)
+	}
+}
 
 func main() {
 	e := echo.New()
@@ -20,11 +54,7 @@ func main() {
 	e.Static("/static", "assets")
 
 	e.GET("/hello/:user", hello)
+	e.GET("/ws", helloWs)
 
 	e.Logger.Fatal(e.Start(":1323"))
-}
-
-func hello(c echo.Context) error {
-	u := c.Param("user")
-	return c.String(http.StatusOK, "hello, "+u)
 }
